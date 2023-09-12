38b041253ec780585c585d6abd9f5c8c4f6c56e3
renyu111 <renyuikissyou@163.com>
2023-09-12 14:26:36 +0800

    feat: 添加cssinjs

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,10 +1,23 @@
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
+import { css } from "@emotion/css";
 export default defineComponent({
-
   setup() {
-    return () =>  <div  style={{
-      backgroundColor:"pink",
-      color:"green"
-    }}>header</div>
-  }
-})
+    return () => <div class={css`
+      background-color: pink;
+      color: green;
+      &:hover{
+        background-color: aqua;
+        color: chartreuse;
+      }
+      @media (min-width: 420px) {
+        font-size: 50px;
+      }
+      @media (min-width: 760px) {
+        font-size: 60px;
+      }
+      @media (min-width: 1280px) {
+        font-size: 80px;
+      }
+    `}>header</div>;
+  },
+});
